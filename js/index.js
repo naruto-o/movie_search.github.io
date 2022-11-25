5e6c96e056bdb9eefecab96f9b3e4b5e438253be
Shikhar Sharma <73755581+naruto-o@users.noreply.github.com>
2022-11-25 22:46:14 +0530

    Add files via upload
    This website is for hackerblocks exam for web development in movie search recommendation

---
+++ js/index.js
@@ -0,0 +1,16 @@
+console.log("hello");
+function functiona1(){
+    document.body.style.backgroundColor="grey";
+}
+function functiona2(){
+    document.body.style.backgroundColor="pink";
+}
+function functiona3(){
+    document.body.style.backgroundColor="blue";
+}
+function functiona4(){
+    document.body.style.backgroundColor="green";
+}
+function functiona5(){
+    document.body.style.backgroundColor="orange";
+}
